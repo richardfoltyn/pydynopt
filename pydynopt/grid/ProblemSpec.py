a9c3aeb4c34259b97d9f594bbd5c7af1060b55ae
Richard Foltyn <richard.foltyn@gmail.com>
2014-01-16 11:44:10 +0100

    Initial commit

---
+++ pydynopt/grid/ProblemSpec.py
@@ -0,0 +1,15 @@
+__author__ = 'richard'
+
+
+class ProblemSpec:
+    def __init__(self):
+        pass
+
+    def actions(self, iState):
+        pass
+
+    def transitions(self, iState, actions):
+        pass
+
+    def util(self, iState, actions):
+        pass
