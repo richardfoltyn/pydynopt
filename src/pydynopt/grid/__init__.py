af856833ed541f29458b01f00720fa7c207a0edd
Richard Foltyn <richard.foltyn@gmail.com>
2014-02-14 14:15:36 +0100

    Rename specs.py to more descriptive name.

--- src/pydynopt/grid/__init__.py
+++ src/pydynopt/grid/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from .specs import ProblemSpecExogenous
+from .problems import ProblemSpecExogenous
 from .results import DynoptResult
 from .solvers import vfi, pfi
 
