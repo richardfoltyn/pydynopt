71a5f36122c3093c5e2f98284c1823ff8b641448
Richard Foltyn <richard.foltyn@gmail.com>
2014-02-15 21:54:13 +0100

    Change opt_choice array in DynoptResult such that it always has at least grid_shape + 1 dimensions. Not squeezing the actions dimension if there is only one sort of action makes code more predictable.

--- src/pydynopt/grid/results.py
+++ src/pydynopt/grid/results.py
@@ -6,13 +6,10 @@ class DynoptResult(object):
     def __init__(self, ps, v, opt_choice, iters, tol, transm=None):
         self._v = v.reshape(ps.grid_shape)
         # Policy functions might contain more than one choice variable,
-        # which will be the last dimension of this thing. Reshape accordingly.
-        # Squeeze the policy function dimension if there is only one choice
-        # variable.
-        if len(opt_choice.shape) > 2 and opt_choice.shape[2] > 1:
-            shp = ps.grid_shape + (-1,)
-        else:
-            shp = ps.grid_shape
+        # which will be the last dimension of this thing.
+        # It's more consistent to keep the action dimension around even if
+        # there is only one.
+        shp = ps.grid_shape + (-1,)
         self._opt_choice = opt_choice.reshape(shp)
         self._iters, self._tol = iters, tol
         self._transm = transm
