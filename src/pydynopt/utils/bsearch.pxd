41533355f5bbb4485249c5d15e974860a1692b52
Richard Foltyn <richard.foltyn@gmail.com>
2015-01-12 10:25:17 +0100

    Remove except statement from _bsearch

--- src/pydynopt/utils/bsearch.pxd
+++ src/pydynopt/utils/bsearch.pxd
@@ -2,8 +2,8 @@
 from ..common.types cimport int_real_t
 
 cpdef unsigned long _bsearch(int_real_t[:] arr, int_real_t key,
-                            bint first) nogil except -1
+                            bint first) nogil
 
 cdef unsigned long _bsearch_impl(int_real_t[:] arr, int_real_t key,
                             unsigned long lb, unsigned long ub,
-                            bint first) nogil except -1
+                            bint first) nogil
--- src/pydynopt/utils/bsearch.pyx
+++ src/pydynopt/utils/bsearch.pyx
@@ -12,7 +12,7 @@ from ..common.types cimport int_real_t
 @cdivision(True)
 cdef unsigned long _bsearch_impl(int_real_t[:] arr, int_real_t key,
                             unsigned long lb, unsigned long ub,
-                            bint first) nogil except -1:
+                            bint first) nogil:
 
     if arr[0] > key:
         return -1
@@ -34,7 +34,7 @@ cdef unsigned long _bsearch_impl(int_real_t[:] arr, int_real_t key,
 
 
 cpdef unsigned long _bsearch(int_real_t[:] arr, int_real_t key,
-                             bint first) nogil except -1:
+                             bint first) nogil:
 
     cdef unsigned long ifrom = 0, ito = arr.shape[0] - 1
 
