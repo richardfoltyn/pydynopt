9607c40f91798d6077af725de7f5dba12bfc919d
Richard Foltyn <richard.foltyn@gmail.com>
2015-01-21 16:47:11 +0100

    Add previously deleted unit tests.

---
+++ src/tests/utils/bsearch.py
@@ -0,0 +1,122 @@
+from __future__ import division, absolute_import, print_function
+__author__ = 'Richard Foltyn'
+
+import numpy as np
+import unittest2 as ut
+
+from pydynopt.utils import bsearch, BSearchFlag, bsearch_eq
+
+
+class BSearchTest(ut.TestCase):
+
+    def test_random(self):
+        """
+        Run tests with randomly generated arrays of floats, using both unique
+        and non-unique array values.
+        """
+
+        # include some pathological array sizes in tests
+        for n in (1, 2, 3, 10, 101):
+            arr1 = np.random.uniform(low=0, high=100, size=(n,))
+            arr1 = np.sort(arr1)
+
+            # test first/last mechanism for repeated values
+            arr2 = np.repeat(arr1, 2)
+
+            for arr in (arr1, arr2):
+
+                if n <= 10:
+                    # for small arrays, test for all indices
+                    idx = np.arange(n)
+                else:
+                    idx = np.random.randint(low=0, high=arr.shape[0] - 1, size=(10, ))
+
+                for key in arr[idx]:
+                    i = bsearch_eq(arr, key, BSearchFlag.first)
+                    j = np.min(np.where(np.logical_and(arr <= key, arr >= key))[0])
+                    self.assertEqual(i, j)
+
+                    i = bsearch_eq(arr, key, BSearchFlag.last)
+                    j = np.max(np.where(np.logical_and(arr <= key, arr >= key))[0])
+                    self.assertEqual(i, j)
+
+    def test_first_last(self):
+        """
+        Test whether returned index is the same for 'first' and 'last' if
+        values in array are unique.
+        """
+        for n in (1, 2, 3, 10, 101):
+            arr = np.arange(n)
+
+            # Exact matches
+            for key in arr:
+                first = bsearch_eq(arr, key, BSearchFlag.first)
+                last = bsearch_eq(arr, key, BSearchFlag.last)
+                self.assertEqual(first, last)
+
+            # < matches
+            for key in (1.5, 2.5, 10.5):
+                first = bsearch_eq(arr, key, BSearchFlag.first)
+                last = bsearch_eq(arr, key, BSearchFlag.last)
+                self.assertEqual(first, last)
+
+    def test_value_error(self):
+        """
+        Check whether ValueError is raised when passing a key that is smaller
+        than first element in array.
+        """
+        arr = np.arange(10, 20)
+        self.assertRaises(ValueError, bsearch_eq, arr=arr, key=1)
+
+    def test_equal(self):
+
+        for n in (1, 2, 3, 10, 101):
+            arr = np.ones((n,), dtype=np.float)
+
+            i = bsearch_eq(arr, 1.0, BSearchFlag.first)
+            self.assertEqual(i, 0)
+
+            i = bsearch_eq(arr, 1.0, BSearchFlag.last)
+            self.assertEqual(i, n - 1)
+
+    def test_bsearch_last_random(self):
+        # include some pathological array sizes in tests
+        for n in (1, 2, 3, 10, 101):
+            arr1 = np.random.uniform(low=0, high=100, size=(n,))
+            arr1 = np.sort(arr1)
+
+            # add repeated values
+            arr2 = np.repeat(arr1, 2)
+
+            for arr in (arr1, arr2):
+
+                if n <= 10:
+                    # for small arrays, test for all indices
+                    idx = np.arange(n)
+                else:
+                    idx = np.random.randint(low=0, high=arr.shape[0] - 1, size=(10, ))
+
+                for key in arr[idx]:
+                    i = bsearch(arr, key)
+                    j = np.max(np.where(arr <= key)[0])
+                    self.assertEqual(i, j)
+
+    def test_bsearch_last_boundaries(self):
+        for n in (1, 2, 3, 10, 101):
+            arr = np.sort(np.random.uniform(low=1, high=100, size=(n,)))
+
+            # should get -1 if key < arr[0]
+            i = bsearch(arr, arr[0] - 10)
+            self.assertEqual(i, -1)
+
+            # should get arr.shape[0] if key > arr[-1]
+            i = bsearch(arr, arr[-1] + 10)
+            self.assertEqual(i, arr.shape[0])
+
+            # should get arr.shape[0] - 1 if key = arr[-1]
+            i = bsearch(arr, arr[-1])
+            self.assertEqual(i, arr.shape[0] - 1)
+
+            # should get 0 if key == arr[0]
+            i = bsearch(arr, arr[0])
+            self.assertEqual(i, 0)
---
+++ src/tests/utils/cartesian.py
@@ -0,0 +1,134 @@
+from __future__ import division, absolute_import, print_function
+__author__ = 'Richard Foltyn'
+
+import numpy as np
+import unittest2 as ut
+
+from pydynopt.utils import cartesian, cartesian2d, _cartesian2d
+
+
+# Benchmark method implemented in python
+def py_cartesian(a, b):
+    a = np.atleast_2d(a)
+    b = np.atleast_2d(b)
+    b1 = np.tile(b, (1, a.shape[1]))
+    a1 = np.repeat(a, b.shape[1], axis=1)
+
+    return np.vstack((a1, b1))
+
+
+class CartesianTest(ut.TestCase):
+
+    def get_arrays(self, dtype=np.int64):
+        a = np.arange(15, dtype=dtype).reshape((3, -1))
+        b = np.arange(6, dtype=dtype).reshape((2, -1))
+
+        return a, b
+
+    def check_dims(self, a, b, ab):
+        a = np.atleast_2d(a)
+        b = np.atleast_2d(b)
+        self.assertEqual(ab.shape[0], a.shape[0] + b.shape[0])
+        self.assertEqual(ab.shape[1], a.shape[1] * b.shape[1])
+
+    def test_2d(self):
+
+        a, b = self.get_arrays()
+
+        ab1 = py_cartesian(a, b)
+        ab2 = cartesian2d(a, b)
+
+        self.assertTrue(np.all(ab1.shape == ab2.shape))
+        self.check_dims(a, b, ab1)
+        self.assertTrue(np.all(ab1 == ab2))
+
+    def test_types(self):
+        for t in (np.float32, np.float64, np.int16, np.int32, np.int64):
+            a, b = self.get_arrays(dtype=t)
+
+            ab = cartesian2d(a, b)
+            self.assertEqual(t, ab.dtype)
+
+    def test_1d(self):
+        a = np.arange(10)
+        b = np.arange(11, 15)
+
+        ab1 = cartesian(a, b)
+        ab2 = py_cartesian(a, b)
+
+        self.check_dims(a, b, ab1)
+        self.assertTrue(np.all(ab1.shape == ab2.shape))
+        self.assertTrue(np.all(ab1 == ab2))
+
+    def test_contiguous(self):
+        a = np.ascontiguousarray(np.arange(15).reshape((3, -1)))
+        b = np.ascontiguousarray(np.arange(6).reshape((2, -1)))
+
+        ab = cartesian2d(a, b)
+
+        self.assertTrue(np.isfortran(ab.T))
+
+        a = np.array(np.arange(15).reshape((3, -1)), order='F')
+        b = np.array(np.arange(6).reshape((2, -1)), order='F')
+
+        ab = cartesian2d(a.T, b.T)
+
+        self.assertTrue(np.isfortran(ab.T))
+
+    def test_pathological(self):
+        """
+        Test some weird arrays which make little sense to pass as arguments
+        """
+
+        a = np.array([[1]])
+        b = np.array([[1]])
+
+        ab1 = cartesian2d(a, b)
+        ab2 = py_cartesian(a, b)
+
+        self.assertTrue(np.all(ab1.shape == ab2.shape))
+        self.assertTrue(np.all(ab1 == ab2))
+        self.check_dims(a, b, ab1)
+
+        a = np.array([1])
+        b = np.array([2])
+
+        ab1 = cartesian(a, b)
+        ab2 = py_cartesian(a, b)
+
+        self.assertItemsEqual(ab1.shape, ab2.shape)
+        self.assertItemsEqual(ab1, ab2)
+        self.check_dims(a, b, ab1)
+
+    def test_out(self):
+        """
+        Test whether passing `out` argument works.
+        """
+
+        a, b = self.get_arrays()
+        ab2 = py_cartesian(a, b)
+        ab1 = np.empty_like(ab2)
+
+        cartesian2d(a, b, ab1)
+
+        self.assertItemsEqual(ab1.shape, ab2.shape)
+        self.assertTrue(np.all(ab1 == ab2))
+
+    def test_out_dims(self):
+        """
+        Test whether function aborts when output array has wrong dimensions.
+        """
+
+        a, b = self.get_arrays()
+        ab1 = py_cartesian(a, b)
+
+        # Create various combinations of misspecified shapes
+        out1 = np.empty((ab1.shape[0] - 1, ab1.shape[1] - 1), dtype=a.dtype)
+        out2 = np.empty((ab1.shape[0] - 1, ab1.shape[1] - 0), dtype=a.dtype)
+        out3 = np.empty((ab1.shape[0] - 0, ab1.shape[1] - 1), dtype=a.dtype)
+
+        for out in (out1, out2, out3):
+            self.assertRaises(ValueError, cartesian2d, a=a, b=b, out=out)
+
+            retval = _cartesian2d(a, b, out)
+            self.assertEqual(retval, -1)
