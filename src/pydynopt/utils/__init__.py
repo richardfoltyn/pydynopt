b2ea0dde4302067b29d43de46adde2556b453d6f
Richard Foltyn <richard.foltyn@gmail.com>
2015-01-21 16:52:13 +0100

    Insert erroneously deleted import.

--- src/pydynopt/utils/__init__.py
+++ src/pydynopt/utils/__init__.py
@@ -2,9 +2,8 @@ from __future__ import absolute_import, print_function
 
 from .distributions import normprob
 
-# from .interpolate import vinterp, interp
 from .diagnostics import print_factory
-
 from .cartesian import cartesian, cartesian2d, _cartesian2d
-
 from .bsearch import bsearch, BSearchFlag, bsearch_eq
+
+from .gridops import makegrid
