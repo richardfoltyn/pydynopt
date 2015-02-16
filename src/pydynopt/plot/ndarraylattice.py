f320426cd49af2f90ddf98b538033a571be11fbf
Richard Foltyn <richard.foltyn@gmail.com>
2015-02-16 22:23:30 +0100

    Start refactoring array plotting again.

--- src/pydynopt/plot/ndarraylattice.py
+++ src/pydynopt/plot/ndarraylattice.py
@@ -13,91 +13,72 @@ from .baseplots import plot_grid
 
 class PlotDimension(object):
 
-    def __init__(self, dim=None, at=None, values=None,
-                 label_fmt=None, label=None, label_loc='lower right'):
+    def __init__(self, dim=None, at_idx=None, at_val=None, values=None,
+                 label_fmt=None, label=None, label_loc='lower right',
+                 label_fun=None):
+
         self.dim = dim
         self.label_fmt = label_fmt
         self.label_loc = label_loc
         self.label = label
+        self.label_fun = label_fun
+
+        if at_val is not None and values is None:
+            raise ValueError('Argument values must not be None if at_val '
+                             'specified')
 
         if dim is not None:
-            if at is not None:
-                if isinstance(at, slice):
-                    step = at.step
-                    at = np.arange(at.start, at.stop, step)
+            if values is not None:
+                values = np.sort(np.atleast_1d(values))
+            if at_idx is not None:
+                if isinstance(at_idx, slice):
+                    step = at_idx.step
+                    at_idx = np.arange(at_idx.start, at_idx.stop, step)
                 else:
-                    at = np.array(at, dtype=np.int)
-
-                if values is not None:
-                    values = np.atleast_1d(values)
-                    if len(values) != len(at):
-                        values = np.atleast_1d(values[at])
-                else:
-                    values = (None, ) * len(at)
-            elif values is not None:
-                values = np.atleast_1d(values)
-                at = np.arange(len(values))
-        else:
-            at = (None, )
-            values = (None, )
-
-        self.values = values
-        self.at = tuple(at)
-
-    def __len__(self):
-        return len(self.at)
-
-    def __iter__(self):
-        return zip(self.at, self.values)
-
-
-class PlotLayer(object):
-
-    def __init__(self, dim=None, at=None, values=None, label=None,
-                 label_fmt=None, **kwargs):
-
-        self.label_fmt = label_fmt
-        self.label = label
-        self.plot_kw = kwargs
-
-        if dim is not None:
-            dim = tuple(np.atleast_1d(dim))
-
-            if at is not None:
-                at = tuple(np.atleast_1d(at))
-                if len(at) != len(dim):
-                    raise ValueError('Need to specify array indices for each '
-                                     'dimension')
-            else:
-                raise ValueError('Need to specify array indices for each '
-                                 'dimension')
+                    at_idx = np.atleast_1d(at_idx)
+            if at_val is not None:
+                at_val = np.atleast_1d(at_val)
 
             if values is not None:
-                values = tuple(np.array(values))
-                if len(values) != len(dim):
-                    raise ValueError('Need to specify value for each '
-                                     'dimension')
-            else:
-                values = (None, ) * len(dim)
+                if at_val is not None:
+                    at_idx = np.searchsorted(values, at_val)
+                elif at_idx is not None:
+                    at_val = values[at_idx]
+                else:
+                    at_idx = np.arange(len(values))
+                    at_val = values
 
+            if at_idx is None:
+                raise ValueError('Insufficient information to obtain array '
+                                 'indices for dimension {:d}'.format(dim))
         else:
-            dim = tuple()
-            at = tuple()
-            values = tuple()
+            at_idx = (None, )
+            at_val = (None, )
+            values = (None, )
 
-        self.dim = dim
-        self.at = at
+        self.at_idx = at_idx
+        self.at_val = at_val
         self.values = values
 
     def __len__(self):
-        return len(self.dim)
+        return len(self.at_idx)
 
     def __iter__(self):
-        return zip(self.dim, self.at, self.values)
+        return zip(self.at_idx, self.at_val)
 
-    @property
-    def dim_max(self):
-        return max(self.dim) if self.dim else None
+
+class PlotLayer(PlotDimension):
+
+    def __init__(self, dim=None, at_idx=None, at_val=None, values=None,
+                 label=None, label_fmt=None, label_loc=None, label_fun=None,
+                 **kwargs):
+
+        super(PlotLayer, self).__init__(dim=dim, at_idx=at_idx, at_val=at_val,
+                                        values=values, label=label,
+                                        label_fmt=label_fmt,
+                                        label_loc=label_loc,
+                                        label_fun=label_fun)
+        self.plot_kw = kwargs
 
 
 class PlotMap(object):
@@ -247,21 +228,23 @@ class NDArrayLattice(object):
         self.rows = PlotDimension()
         self.cols = PlotDimension()
         self.xaxis = PlotDimension()
-        self.layers = list()
+        self.layers = PlotLayer()
 
     def map_rows(self, dim, at=None, values=None, label=None, label_fmt=None,
                  label_loc='lower left'):
         self.rows = PlotDimension(dim=dim, at=at, label=label, values=values,
                                   label_fmt=label_fmt, label_loc=label_loc)
 
-    def map_cols(self, dim, at=None, values=None, label=None, label_fmt=None,
+    def map_columns(self, dim, at=None, values=None, label=None, label_fmt=None,
                  label_loc='lower left'):
         self.cols = PlotDimension(dim=dim, at=at, label=label, values=values,
                                   label_fmt=label_fmt, label_loc=label_loc)
 
-    def add_layer(self, dim, at=None, values=None, label=None, label_fmt=None):
+    def map_layers(self, dim, at=None, values=None, label=None,
+                   label_fmt=None, **kwargs):
         self.layers.append(PlotLayer(dim=dim, at=at, values=values,
-                                     label=label, label_fmt=label_fmt))
+                                     label=label, label_fmt=label_fmt,
+                                     **kwargs))
 
     def reset_layers(self):
         self.layers = list()
@@ -286,6 +269,8 @@ class NDArrayLattice(object):
              xlim=None, extend_by=0, identity=False, **kwargs):
         NDArrayLattice._plot(self, data, style, trim_iqr, ylim, xlim,
                              extend_by, identity=identity, **kwargs)
+
+
 
     @staticmethod
     def _plot(nda, data, style=DefaultStyle, trim_iqr=2, ylim=None,
