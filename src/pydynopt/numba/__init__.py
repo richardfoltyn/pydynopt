7e8ab6098c91dbc6e5e709ca077ce55200574b71
Richard Foltyn <richard.foltyn@gmail.com>
2019-08-12 17:32:11 +0200

    numba: Add @register_jitable to numba proxy module

--- src/pydynopt/numba/__init__.py
+++ src/pydynopt/numba/__init__.py
@@ -2,5 +2,5 @@
 Author: Richard Foltyn
 """
 
-from .wrapper import jit, jitclass, overload
+from .wrapper import jit, jitclass, overload, register_jitable
 from .wrapper import int64, float64, boolean, string
--- src/pydynopt/numba/wrapper.py
+++ src/pydynopt/numba/wrapper.py
@@ -7,7 +7,8 @@ from pydynopt import use_numba
 
 import numpy as np
 
-__all__ = ['jit', 'jitclass', 'overload', 'float64', 'int64', 'boolean']
+__all__ = ['jit', 'jitclass', 'overload', 'register_jitable',
+           'float64', 'int64', 'boolean']
 
 
 def jit(signature_or_function=None, *jit_args, **jit_kwargs):
@@ -51,7 +52,7 @@ def jitclass(spec):
     return decorate
 
 
-def overload(func, *overload_args, **overload_kwargs):
+def overload(func, jit_options={}, strict=True):
     """
     Default implementation of Numba's @overload decorator when Numba is
     not available or not desired.
@@ -63,6 +64,34 @@ def overload(func, *overload_args, **overload_kwargs):
         return wrapper
 
     return decorate
+
+
+def register_jitable(*args, **kwargs):
+    """
+    Register a regular python function that can be executed by the python
+    interpreter and can be compiled into a nopython function when referenced
+    by other jit'ed functions.  Can be used as::
+        @register_jitable
+        def foo(x, y):
+            return x + y
+    Or, with compiler options::
+        @register_jitable(_nrt=False) # disable runtime allocation
+        def foo(x, y):
+            return x + y
+    """
+
+    def wrap(fn):
+        # It is just a wrapper for @overload
+        @overload(fn, jit_options=kwargs, strict=False)
+        def ov_wrap(*args, **kwargs):
+            return fn
+
+        return fn
+
+    if kwargs:
+        return wrap
+    else:
+        return wrap(*args)
 
 
 class SubscriptableType(np.int64):
@@ -82,7 +111,7 @@ boolean = SubscriptableType()
 if use_numba:
     try:
         from numba import jit, jitclass
-        from numba.extending import overload
+        from numba.extending import overload, register_jitable
         from numba.types import int64, float64, boolean, string
     except ImportError:
         # Nothing to do, use the default decorators defined above
