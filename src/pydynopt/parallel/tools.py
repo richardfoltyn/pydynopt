2884ff72b529321ba814e18256a46221f3d78443
Richard Foltyn <richard.foltyn@gmail.com>
2019-08-13 21:00:21 +0200

    parallel: Improve chunk_sizes(), add Numba support

--- src/pydynopt/parallel/tools.py
+++ src/pydynopt/parallel/tools.py
@@ -1,30 +1,49 @@
-from __future__ import print_function, division, absolute_import
 
-__author__ = 'Richard Foltyn'
-
+from pydynopt.numba import register_jitable
 import numpy as np
 
 
-def chunk_sizes(nthreads, ntasks, bounds=False):
+@register_jitable
+def chunk_sizes(nthreads, ntasks):
+    """
+    Split a given number of tasks (almost) uniformly across the given number
+    of threads. This function takes care of the case when it is not possible
+    to assign the same number of tasks to each thread.
+
+    Parameters
+    ----------
+    nthreads : int
+        Number of threads
+    ntasks : int
+        Number of task to be divided across threads
+
+    Returns
+    -------
+    chunks : np.ndarray
+        Array containing the number of task for each thread
+    istart : np.ndarray
+        Start index of the chunk assigned to each thread
+    iend : np.ndarray
+        (non-inclusive) end index of the chunk assigned to each thread
+    """
 
     nthreads = int(nthreads)
     ntasks = int(ntasks)
 
     if nthreads < 1:
-        raise ValueError('Invalid number of threads: {:d}'.format(nthreads))
+        raise ValueError('Invalid number of threads')
 
     base = ntasks // nthreads
     rest = ntasks % nthreads
 
-    chunks = np.ones((nthreads, ), dtype=np.uint) * base
+    chunks = np.ones((nthreads, ), dtype=np.int64) * base
     chunks[:rest] += 1
 
     assert np.sum(chunks) == ntasks
 
-    if not bounds:
-        return chunks
-
     iend = np.cumsum(chunks)
-    istart = np.hstack((np.array(0, dtype=np.uint), iend[:-1]))
+    istart = np.empty_like(iend)
+    istart[0] = 0
+    istart[1:] = iend[:-1]
 
     return chunks, istart, iend
