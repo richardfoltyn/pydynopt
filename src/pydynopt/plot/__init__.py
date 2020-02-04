c53ef82cbd9f8cbf9257a60530fb6b2ac4b197b9
Richard Foltyn <richard.foltyn@gmail.com>
2020-02-04 22:35:54 +0100

    plots: Import QualitativeStyle in __init__.py

--- src/pydynopt/plot/__init__.py
+++ src/pydynopt/plot/__init__.py
@@ -4,3 +4,4 @@ from .baseplots import plot_grid
 from .ndarraylattice import NDArrayLattice
 from .plotmap import PlotMap, plot_pm
 from .styles import DefaultStyle, Presentation, PurpleBlue, AlternatingStyle
+from .styles import QualitativeStyle
