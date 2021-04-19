3fc596911898540490411d10d419e5bd8cc6b10f
Richard Foltyn <richard.foltyn@gmail.com>
2021-04-19 20:32:31 +0200

    processes: Export markow_simulate from top-level package

--- src/pydynopt/processes/__init__.py
+++ src/pydynopt/processes/__init__.py
@@ -1,5 +1,6 @@
 __author__ = 'Richard Foltyn'
 
 from .processes import markov_ergodic_dist, markov_moments, rouwenhorst
+from .processes import markov_simulate
 from .processes import istransm
 from .tauchen import tauchen
