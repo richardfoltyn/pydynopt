d69ad43a6bd407504f4d33fe2e65bcfd91dcd31c
Richard Foltyn <richard.foltyn@gmail.com>
2024-03-23 16:14:59 +0100

    plot: Add default formatting option to SuffixFormatter, change to class

--- src/pydynopt/plot/ticker.py
+++ src/pydynopt/plot/ticker.py
@@ -4,50 +4,56 @@ https://creativecommons.org/licenses/by/4.0/
 
 Author: Richard Foltyn
 """
+from typing import Optional
 
 from matplotlib.ticker import FuncFormatter
 
 
-def _suffix_formatter(value: float, pos: int) -> str:
-    """
-    Format numbers with suffixes k, m, or bn depending
-    on their magnitue.
+class SuffixFormatter(FuncFormatter):
 
-    Parameters
-    ----------
-    value : float
-        Tick value to format
-    pos : int
-        Tick position within list of tick labels
+    def __init__(self, default: Optional[str] = None):
 
-    Returns
-    -------
-    str
-        Formatted tick label
-    """
-    suffix = ''
-    if value >= 1.0e12:
-        value /= int(1.0e12)
-        suffix = 'tr'
-    elif value >= 1.0e9:
-        value /= int(1.0e9)
-        suffix = 'bn'
-    elif value >= 1.0e6:
-        value /= int(1.0e6)
-        suffix = 'm'
-    elif value >= 1000:
-        value /= 1000
-        suffix = 'k'
+        def _suffix_formatter(value: float, pos: int) -> str:
+            """
+            Format numbers with suffixes k, m, or bn depending
+            on their magnitue.
 
-    if int(value) == value:
-        fmt = '.0f'
-    else:
-        fmt = ''
+            Parameters
+            ----------
+            value : float
+                Tick value to format
+            pos : int
+                Tick position within list of tick labels
 
-    fmt = f'{{v:{fmt}}}{suffix}'
+            Returns
+            -------
+            str
+                Formatted tick label
+            """
+            suffix = ''
+            if value >= 1.0e12:
+                value /= int(1.0e12)
+                suffix = 'tr'
+            elif value >= 1.0e9:
+                value /= int(1.0e9)
+                suffix = 'bn'
+            elif value >= 1.0e6:
+                value /= int(1.0e6)
+                suffix = 'm'
+            elif value >= 1000:
+                value /= 1000
+                suffix = 'k'
 
-    return fmt.format(v=value)
+            if int(value) == value:
+                fmt = '.0f'
+            elif not suffix and default is not None:
+                fmt = default
+            else:
+                fmt = ''
 
+            fmt = f'{{v:{fmt}}}{suffix}'
 
-SuffixFormater = FuncFormatter(_suffix_formatter)
+            return fmt.format(v=value)
+
+        super().__init__(_suffix_formatter)
 
