a89cabe25286d958ae42bce6c499ceb8ff9dab0a
Richard Foltyn <richard.foltyn@gmail.com>
2026-09-05 11:01:01 +0200

    interpolate: Modernize docstrings & ruff/ty checks

--- src/pydynopt/interpolate/__init__.py
+++ src/pydynopt/interpolate/__init__.py
@@ -1,8 +1,29 @@
 """
+Interpolation routines and numerical search functions.
+
+- 1D linear and 2D bilinear interpolation routines
+- Bracket location and interpolant evaluation functions
+
 This work is licensed under CC BY 4.0,
 https://creativecommons.org/licenses/by/4.0/
 
 Author: Richard Foltyn
 """
 
-from .linear import *
+from .linear import (
+    interp1d,
+    interp1d_eval,
+    interp1d_locate,
+    interp2d,
+    interp2d_eval,
+    interp2d_locate,
+)
+
+__all__ = [
+    'interp1d',
+    'interp1d_eval',
+    'interp1d_locate',
+    'interp2d',
+    'interp2d_eval',
+    'interp2d_locate',
+]
