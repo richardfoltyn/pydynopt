9ed6fa2e165c1c0ee2a8494c7ca80d82d872b24b
Richard Foltyn <richard.foltyn@gmail.com>
2026-09-05 10:32:17 +0200

    arrays: Modernize docstrings & ruff formatting & ty checks

--- src/pydynopt/arrays/__init__.py
+++ src/pydynopt/arrays/__init__.py
@@ -1,10 +1,23 @@
 """
+Routines for creating, transforming, and indexing multidimensional arrays.
+
+- Grid generation functions (power-spaced, log-spaced)
+- Coordinate and linear index conversion (ind2sub, sub2ind)
+- Probability clipping routines
+
 This work is licensed under CC BY 4.0,
 https://creativecommons.org/licenses/by/4.0/
 
 Author: Richard Foltyn
 """
 
-
 from .base import clip_prob, logspace, powerspace
 from .indexing import ind2sub, sub2ind
+
+__all__ = [
+    'clip_prob',
+    'ind2sub',
+    'logspace',
+    'powerspace',
+    'sub2ind',
+]
