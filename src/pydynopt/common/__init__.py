59081d529138e3c1a982361f5d8d906c3546b387
Richard Foltyn <richard.foltyn@gmail.com>
2026-09-05 10:51:06 +0200

    common: Modernize docstrings & ruff formatting

--- src/pydynopt/common/__init__.py
+++ src/pydynopt/common/__init__.py
@@ -1,4 +1,8 @@
 """
+Common utilities and shared exceptions.
+
+- Convergence error definitions
+
 This work is licensed under CC BY 4.0,
 https://creativecommons.org/licenses/by/4.0/
 
@@ -6,3 +10,7 @@ Author: Richard Foltyn
 """
 
 from .errors import ConvergenceError
+
+__all__ = [
+    'ConvergenceError',
+]
