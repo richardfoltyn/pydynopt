37db8fbc487a43df4576502518e567701fb966f5
Richard Foltyn <richard.foltyn@gmail.com>
2026-09-05 10:51:06 +0200

    common: Modernize docstrings & ruff formatting

--- src/pydynopt/common/errors.py
+++ src/pydynopt/common/errors.py
@@ -1,21 +1,54 @@
 """
+Exception classes for numerical and optimization routines.
+
+- Convergence failure errors
+
 This work is licensed under CC BY 4.0,
 https://creativecommons.org/licenses/by/4.0/
 
 Author: Richard Foltyn
 """
 
+__all__ = [
+    'ConvergenceError',
+]
 
-class ConvergenceError(BaseException):
 
-    def __init__(self, iterations, tol):
+class ConvergenceError(Exception):
+    """
+    Exception raised when an iterative algorithm fails to converge.
+
+    Parameters
+    ----------
+    iterations
+        Number of iterations performed before convergence failure.
+    tol
+        Achieved tolerance or error metric at termination.
+    """
+
+    def __init__(self, iterations: int, tol: float) -> None:
+        super().__init__(iterations, tol)
         self._iters = iterations
         self._tol = tol
 
     @property
-    def iterations(self):
+    def iterations(self) -> int:
+        """
+        Number of iterations performed.
+
+        Returns
+        -------
+        Number of iterations performed before convergence failure.
+        """
         return self._iters
 
     @property
-    def tol(self):
+    def tol(self) -> float:
+        """
+        Achieved tolerance or error metric.
+
+        Returns
+        -------
+        Achieved tolerance or error metric at termination.
+        """
         return self._tol
